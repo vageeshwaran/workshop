88d1abc88fbc479c7cf03334839823f770f20d5f
vageeshwaran <vageeshwaran1@gmail.com>
2024-09-20 16:42:51 +0530

    commited

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 guest=["dinesh", "deva", "sehwak", "sudharson", "tamizh", "kharthi"]
 for i in guest:
     print(f"hi {i}, you are invited to dinner")
-print("sorry, only two people can attend the dinner at a time")
+print("sorry, only two people can attend the dinner at a  time")
