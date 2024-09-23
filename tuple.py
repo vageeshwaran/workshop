2c84f2344d24eeea37c90470096e6606eed1d03e
vageeshwaran <vageeshwaran1@gmail.com>
2024-09-23 16:41:01 +0530

    empty

---
+++ tuple.py
@@ -0,0 +1,9 @@
+buffet=("idli", "chapathi", "parotta", "dosa", "noodles", "fried rice")
+for food in buffet:
+    print(food)
+    
+    
+print("\n revised menu")
+buffet=("idli", "chapathi", "parotta", "dosa", "upma", "poori")
+for food in buffet:
+    print(food)
